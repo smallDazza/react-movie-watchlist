3cc201d3fe9caaba8117f250360a5f3381f7822e
smallDazza <smalldazza@gmail.com>
2024-12-05 21:05:02 +1100

    Feat: added a watchlist with remove function

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
+import WatchlistProvider from './context/WatchlistContext.jsx';
+import Watchlist from './pages/WatchList.jsx';
 
 function App() {
 
   return (
+    <WatchlistProvider>
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/watchlist' element={<div>Watchlist</div>} />
+        <Route path='/watchlist' element={<Watchlist />} />
         <Route path='/details/:id' element={<div>Details</div>} />
       </Routes>
     </Router>
+    </WatchlistProvider>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,53 @@
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 export default function Home() {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
 
-  const fetchMovies = async () => {
-    // Using fetch()
-    // const response = await fetch(`http://www.omdbapi.com/${apikey}/${query}`);
+    const fetchMovies = async () => {
+        // Using fetch()
+        // const response = await fetch(`http://www.omdbapi.com/${apikey}/${query}`); 
 
-    // Using axios()
-    if (query) {
-      const response = await axios.get(`http://www.omdbapi.com/`, {
-        params: {
-          apikey: "add4d0d",
-          s: query,
-        },
-      });
-      console.log(response.data);
-      setMovies(response.data.Search || []);
-    }
-  };
+        // Using axios()
+        if (query) {
+            const response = await axios.get(`http://www.omdbapi.com/`, {
+                params: {
+                    apikey: 'd58efa1d',
+                    s: query
+                }
+            });
+            console.log(response.data);
+            setMovies(response.data.Search || []);
+        }
+    };
 
-  return (
-    <div className="container my-4">
-      <h1 className="text-center mb-4">Movie Search</h1>
-      <div className="row">
-        <div className="col-md-8 offset-md-4">
-          <input
-            type="text"
-            placeholder="Search for Movies..."
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <button
-            className="btn btn-primary btn-block mt-2"
-            onClick={fetchMovies}
-          >
-            Search
-          </button>
+    return (
+        <div className="container my-4">
+
+            <h1 className="text-center mb-4">Movie Search</h1>
+            <div className="row">
+                <div className="col-md-8 offset-md-4">
+                    <input
+                        type="text"
+                        placeholder="Search for Movies..."
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                    <button className="btn btn-primary btn-block mt-2" onClick={fetchMovies}>Search</button>
+                </div>
+
+                <Link to="/watchlist">Watchlist</Link>
+
+            </div>
+            <div className="row mt-4">
+                {
+                    movies.map((movie) => (
+                        <MovieCard key={movie.imdbID} movie={movie} />
+                    ))
+                }
+            </div>
         </div>
-      </div>
-      <div className="row mt-4">
-        {movies.map((movie) => (
-          <MovieCard key={movie.imdbID} movie={movie} />
-        ))}
-      </div>
-    </div>
-  );
+    )
 }
